704460eaadafe8c65627b21a0d4ea2bce5a7f7ea
Lefrec <blackfox244@gmail.com>
2024-12-03 16:29:08 +0100

    using dataset to change img

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,15 @@ console.log("connected");
 const lightBox = document.querySelector("#lightbox");
 console.log(lightBox);
 
+const lightBoxImg = document.querySelector("#lightbox>img");
+console.log(lightBoxImg);
+
 const smallImg = document.querySelectorAll("li>img");
 console.log(smallImg);
 
 smallImg.forEach(element => {
     element.addEventListener("click", () => {
+        lightBoxImg.src = element.dataset.fullImg;
         lightBox.showModal()
     })
 });
