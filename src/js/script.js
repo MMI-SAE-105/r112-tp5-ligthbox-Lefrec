0891cdbd23bd215a42764119ff2068f4c65288ea
Lefrec <blackfox244@gmail.com>
2024-12-03 16:32:08 +0100

    commentaires js

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
+//verifie si le script est bien connecté direct
 console.log("connected");
 
+
+//déclaration des constantes avec les querySelector
+//console.log pour vérifier que tout fonctionne
 const lightBox = document.querySelector("#lightbox");
 console.log(lightBox);
 
@@ -9,6 +13,8 @@ console.log(lightBoxImg);
 const smallImg = document.querySelectorAll("li>img");
 console.log(smallImg);
 
+
+//ajout d'un listener sur chaque miniature qui change l'image de la ligthbox et ouvre la lightbox
 smallImg.forEach(element => {
     element.addEventListener("click", () => {
         lightBoxImg.src = element.dataset.fullImg;
@@ -16,6 +22,8 @@ smallImg.forEach(element => {
     })
 });
 
+
+//listener qui ferme la lightbox
 lightBox.addEventListener("click", () => {
     lightBox.close()
 });
