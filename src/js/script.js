06d1dde5ced7dba007fd08429847306fd2611641
Lefrec <blackfox244@gmail.com>
2024-12-03 16:17:46 +0100

    query selector

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-// *** LIGHTBOX ***
+console.log("connected");
+
+const lightBox = document.querySelector("#lightbox");
+console.log(lightBox);
+
+const smallImg = document.querySelectorAll("li>img");
+console.log(smallImg);
+
